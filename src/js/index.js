c6853914b71093f23224bb6304940b1651571079
Anton Fedorovskyi <anton.fedorovsky@gmail.com>
2018-08-16 20:50:02 +0300

    full copies

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,8 @@ import HeaderCopy from './modules/copy-blocks/header-copy';
 import SubscribePopUp from './modules/subscribePopUp';
 import SocialCopy from './modules/copy-blocks/social-copy';
 import InstagramCopy from './modules/copy-blocks/instagram-copy';
+import FooterCopy from './modules/copy-blocks/footer-copy';
+import ChordCopy from './modules/copy-blocks/chord-copy';
 import '../css/style.css';
 
 const mediaQueries = new MediaQueries();
@@ -26,4 +28,6 @@ $(document).ready(() => {
   new HeaderCopy().init();
   new SocialCopy().init();
   new InstagramCopy().init();
+  new FooterCopy().init();
+  new ChordCopy().init();
 });
