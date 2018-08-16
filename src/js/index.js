2dfec2f58a9e29b0d5253b836799f6790707505f
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2018-08-16 14:35:55 +0300

    add responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,26 @@
 import $ from 'jquery';
 import 'css/style.css';
+import MediaQueries from './modules/mediaQueries';
 import Subscribe from './modules/subscribe';
+
+const mediaQueries = new MediaQueries();
+
+mediaQueries.change('xs', message => console.log('Responsive ->', message));
+mediaQueries.change('sm', message => console.log('Responsive ->', message));
+mediaQueries.change('md', message => console.log('Responsive ->', message));
+mediaQueries.change('lg', message => console.log('Responsive ->', message));
+
 
 $(document).ready(() => {
   new Subscribe().init();
+});
+
+$(document).ready(() => {
+  $('.btn-open').on('click', () => {
+    $('.menu-box').addClass('--state-open');
+  });
+
+  $('.btn-close').on('click', () => {
+    $('.menu-box').removeClass('--state-open');
+  });
 });
---
+++ src/js/modules/mediaQueries.js
@@ -0,0 +1,73 @@
+class MediaQueries {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.xs = window.matchMedia('only screen and (max-width: 767px)');
+    this.sm = window.matchMedia('only screen and (min-width: 768px) and (max-width: 1024px)');
+    this.md = window.matchMedia('only screen and (min-width: 1025px) and (max-width: 1440px)');
+    this.lg = window.matchMedia('only screen and (min-width: 1441px)');
+  }
+
+  change(name, callback) {
+    switch (name) {
+      case 'xs': this.enterXS(callback);
+        break;
+      case 'sm': this.enterSM(callback);
+        break;
+      case 'md': this.enterMD(callback);
+        break;
+      case 'lg': this.enterLG(callback);
+        break;
+      default: this.enterXS(callback);
+    }
+  }
+
+  enterXS(callback) {
+    this.xs.addListener(e => {
+      if (e.matches) {
+        callback('xs');
+      }
+    });
+    if (this.xs.matches) {
+      callback('xs');
+    }
+  }
+
+  enterSM(callback) {
+    this.sm.addListener(e => {
+      if (e.matches) {
+        callback('sm');
+      }
+    });
+    if (this.sm.matches) {
+      callback('sm');
+    }
+  }
+
+  enterMD(callback) {
+    this.md.addListener(e => {
+      if (e.matches) {
+        callback('md');
+      }
+    });
+    if (this.md.matches) {
+      callback('md');
+    }
+  }
+
+  enterLG(callback) {
+    this.lg.addListener(e => {
+      if (e.matches) {
+        callback('lg');
+      }
+    });
+    if (this.lg.matches) {
+      callback('lg');
+    }
+  }
+}
+
+export default MediaQueries;
+
