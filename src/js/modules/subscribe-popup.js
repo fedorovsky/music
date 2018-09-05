e57a534cc852170e50677c0cebe7564a5f8bc26a
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2018-09-05 10:29:04 +0300

    js-phrases

--- src/js/modules/subscribe-popup.js
+++ src/js/modules/subscribe-popup.js
@@ -5,37 +5,33 @@ class SubscribePopup {
     this.subscribeBox = $('#subscribe-main');
     this.btnOpenSubscribe = $('#subscribe-open');
     this.btnCloseSubscribe = $('#subscribe-close');
-    this.phrasesBox = $('#phrases');
-
+    this.phrases = $('.js-phrases');
     this.handlers();
   }
 
   handlers() {
-    this.btnOpenSubscribe.on('click', this.openSubscribe.bind(this));
-    this.btnCloseSubscribe.on('click', this.closeSubscribe.bind(this));
+    this.btnOpenSubscribe.on('click', this.openPopUp.bind(this));
+    this.btnCloseSubscribe.on('click', this.closePopUp.bind(this));
   }
 
-  openSubscribe() {
+  openPopUp() {
     this.subscribeBox.addClass('--state-open');
     this.hideAllPhrases();
     this.showRandomPhrase();
   }
 
-  closeSubscribe() {
+  closePopUp() {
     this.subscribeBox.removeClass('--state-open');
   }
 
   hideAllPhrases() {
-    this.phrasesBox.children().each((i, element) => {
-      $(element).hide();
-    });
+    this.phrases.each((i, element) => $(element).hide());
   }
 
   showRandomPhrase() {
-    const countPhrases = this.phrasesBox.children().length;
+    const countPhrases = this.phrases.length;
     const random = Math.floor(Math.random() * countPhrases);
-    this.phrasesBox
-      .children()
+    this.phrases
       .eq(random)
       .show();
   }
